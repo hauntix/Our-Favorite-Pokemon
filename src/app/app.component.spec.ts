dfa7296800807dd56389d84fa4d9b9f03f478af9
hauntix <c.estrada@email.com>
2019-09-16 20:45:09 -0400

    removed routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'our-favorite-pokemon'`, () => {
+  it(`should have as title 'our-favorite-pokemonv2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('our-favorite-pokemon');
+    expect(app.title).toEqual('our-favorite-pokemonv2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('our-favorite-pokemon app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('our-favorite-pokemonv2 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'our-favorite-pokemon';
+  title = 'our-favorite-pokemonv2';
 }
